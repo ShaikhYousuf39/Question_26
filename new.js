7868248ed5160d8b8b48ef9d3ea72b25d63ab614
YousufSH <160315514+ShaikhYousuf39@users.noreply.github.com>
2024-03-10 01:20:07 +0500

    Add files via upload

---
+++ new.js
@@ -0,0 +1,14 @@
+var alien_color = "green";
+if (alien_color == "green") {
+    console.log("player just earned 5 points for shooting the alien");
+}
+else {
+    console.log("player just earned 10 points");
+}
+var alien_color2 = "red";
+if (alien_color2 == "green") {
+    console.log("player just earned 5 points for shooting the alien");
+}
+else {
+    console.log("player just earned 10 points");
+}
---
+++ new.ts
@@ -0,0 +1,14 @@
+let alien_color:string = "green"
+
+if (alien_color == "green"){
+    console.log("player just earned 5 points for shooting the alien")
+} else{console.log("player just earned 10 points")
+}
+
+let alien_color2:string = "red"
+
+if (alien_color2 == "green"){
+    console.log("player just earned 5 points for shooting the alien")
+} else{console.log("player just earned 10 points")
+}
+
